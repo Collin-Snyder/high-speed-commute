2fc86ea0588a5f90dea7ea8a335197f39eec580a
Collin Snyder <collinjacobsnyder@gmail.com>
2020-03-02 21:04:03 -0600

    jioasdufiosjidof

--- server/index.js
+++ server/index.js
@@ -12,14 +12,24 @@ const {
 
 const app = express();
 
-app.set( 'port', ( process.env.PORT || 5000 ));
+app.set("port", process.env.PORT || 5000);
 
-app.use(express.static("../high-speed-commute/build"));
+app.use(express.static(path.join(__dirname, "/../high-speed-commute/build")));
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/../high-speed-commute/build/static/js"))
+);
+app.use(
+  "/css",
+  express.static(
+    path.join(__dirname, "/../high-speed-commute/build/static/css")
+  )
+);
 app.use(express.json({ limit: "50mb" }));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../high-speed-commute/build/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/../high-speed-commute/build/index.html'))
+// })
 
 app.get("/api/users/:username", (req, res) => {
   getUser(req.params.username, result => res.send(result));
@@ -33,7 +43,7 @@ app.get("/api/levels/:id", (req, res) => {
 
 app.post("/api/users", (req, res) => {
   addUser(req.body.username, result => res.send(result));
-})
+});
 
 app.post("/api/levels", (req, res) => {
   saveNewLevel(req.body, result => {
@@ -60,11 +70,10 @@ app.get("/api/userlevels/:username", (req, res) => {
     getUserLevels(req.params.username, result => {
       levels.userLevels = result;
       res.send(levels);
-    })
-  })
-  
+    });
+  });
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`Now listening on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Now listening on port ${app.get("port")}`);
 });
